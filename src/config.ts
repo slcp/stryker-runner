94b4828a31fdf463808b256aba8ba1222a32c7d8
Stuart Forrest <stuart.forrest@infinityworks.com>
2022-08-26 21:07:08 +0100

    Add the option to use yarn to run Stryker instead of npm

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,16 @@
 import * as vscode from "vscode";
 
-export const strykerCommand = (): string =>
-  vscode.workspace.getConfiguration().get("strykerRunner.stryker.command") ||
-  `npx stryker`;
+export const strykerCommand = (): string => {
+  const defaultCommand = useYarn() ? `yarn stryker` : `npx stryker`;
+  return (
+    vscode.workspace.getConfiguration().get("strykerRunner.stryker.command") ||
+    defaultCommand
+  );
+};
 
 export const strykerConfigFilePath = (): string | undefined =>
   vscode.workspace.getConfiguration().get("strykerRunner.stryker.configFile");
+
+export const useYarn = (): boolean =>
+  vscode.workspace.getConfiguration().get("strykerRunner.node.useYarn") ||
+  false;
